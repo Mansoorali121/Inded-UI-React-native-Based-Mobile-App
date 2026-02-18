9fc82cc5b374de946da9b11b1b6e17ababae1416
Mansoor Ali <mansoorsahito601@gmail.com>
2026-02-18 08:20:15 +0500

    `Refactor MytextInput component: add styles and layout adjustments`

--- src/components/MytextInput.js
+++ src/components/MytextInput.js
@@ -1,19 +1,33 @@
-import { StyleSheet, Text, TextInput, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import React from 'react';
+import { s, vs } from 'react-native-size-matters';
 
-const MytextInput = ({...props}) => {
+const MytextInput = ({ ...props }) => {
   return (
-    <View>
-      <Text>Email Address</Text>
-      <TextInput {...props} 
-      style={styles.input}
-      />
+    <View style={styles.container}>
+      <Text style={styles.email}>Email Address</Text>
+      <TextInput {...props} style={styles.input} />
     </View>
-  )
-}
+  );
+};
 
 export default MytextInput;
 
 const styles = StyleSheet.create({
-    input:{},
-})
+  input: {
+    height: s(50),
+    width: vs(270),
+    backgroundColor: '#fff4f4',
+    borderWidth: s(2),
+    borderColor: 'blue',
+    borderRadius: s(10),
+  },
+  container: { marginHorizontal: s(20), marginTop: s(10) },
+  email: {
+    fontSize: s(12),
+    fontWeight: 'bold',
+    marginBottom: s(10),
+    marginStart: s(5),
+    fontFamily: 'Audiowide-Regular',
+  },
+});
