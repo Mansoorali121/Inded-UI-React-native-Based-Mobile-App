11c04fcc17d8412a9ccf540b52f894d855cd1b00
Mansoor Ali <mansoorsahito601@gmail.com>
2026-02-21 15:38:21 +0500

    `Added SearchButton and styled top container in Home screen`

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,12 +1,21 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import Indedlogo from '../components/Indedlogo';
-import Hometabheader from "../components/Hometabheader";
+import Hometabheader from '../components/Hometabheader';
+import { s } from 'react-native-size-matters';
+import SearchButton from "../components/SearchButton";
 
 const Home = () => {
   return (
     <View style={styles.container}>
-      <Hometabheader/>
+      <Hometabheader />
+      {/* Content of the Blue type Background */}
+      <View style={styles.topcontaienr}>
+        <View style={styles.card}>
+          <Text>Hello Hello </Text>
+          <SearchButton />
+        </View>
+      </View>
     </View>
   );
 };
@@ -14,5 +23,7 @@ const Home = () => {
 export default Home;
 
 const styles = StyleSheet.create({
-  container: { backgroundColor: '#fff',flex:1},
+  container: { backgroundColor: '#fff', flex: 1 },
+  topcontaienr: { marginTop: s(30), marginHorizontal: s(15) },
+  card: { backgroundColor: '#e7fffdc4', height: '100%', borderRadius: s(10) },
 });
