9e4bacdd6d6227a49438d212d95c52695c9b0366
Mansoor Ali <mansoorsahito601@gmail.com>
2026-02-22 13:49:22 +0500

    Search functionality complete

--- src/components/Modalheader.js
+++ src/components/Modalheader.js
@@ -1,20 +1,36 @@
-import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import React from 'react';
+import {
+  Image,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import React, { useEffect , useRef} from 'react';
 import { s } from 'react-native-size-matters';
+import { useNavigation } from '@react-navigation/native';
 
 const Modalheader = () => {
+  const inputRef = useRef(null);
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
   return (
     <View>
       <View style={styles.header}>
         <Text style={styles.headertitle}>Job,title,Keywords or company</Text>
       </View>
-      <View>
-        <TouchableOpacity >
+      <View style={styles.inputcontainer}>
+        <View style={styles.input}>
           <Image
             source={require('../assets/search.png')}
             style={styles.image}
           />
-        </TouchableOpacity>
+          <TextInput  placeholder = "" ref={inputRef} />
+        </View>
       </View>
     </View>
   );
@@ -26,4 +42,14 @@ const styles = StyleSheet.create({
   header: { marginHorizontal: s(15), marginTop: s(40), alignItems: 'center' },
   headertitle: { fontFamily: 'Langar-Regular', fontSize: s(14) },
   image: { height: s(24), width: s(24) },
+  input: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderWidth: s(2),
+    borderColor: 'blue',
+    width: '80%',
+    padding: s(5),
+    borderRadius: s(10),
+  },
+  inputcontainer: { alignItems: 'center', marginTop: s(6) },
 });
